aafe1561d9467fad93995be6bef91d14b73189ed
Rob Verhoef <r.verhoef@hotmail.nl>
2017-05-23 15:50:26 +0200

    Initial implementation Goverment Building

---
+++ Assets/_Core/Scripts/Controllers/GovermentBuildingController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GovermentBuildingController : MonoBehaviour
+{
+    private enum _govermentBuildingStates { Neutral, Peaceful, Defended, Damaged }
+    private _govermentBuildingStates _govermentBuildingState;
+    [SerializeField]
+    private Image _govermentBuildingImage;
+    [SerializeField]
+    private Sprite[] _govermentBuildingSprites;
+
+    void Awake ()
+    {
+        _govermentBuildingState = _govermentBuildingStates.Neutral;
+    }
+
+    private void SetGovermentBuildingSprite ()
+    {
+        switch (_govermentBuildingState)
+        {
+            case _govermentBuildingStates.Neutral:
+                break;
+            case _govermentBuildingStates.Peaceful:
+                break;
+            case _govermentBuildingStates.Defended:
+                break;
+            case _govermentBuildingStates.Damaged:
+                break;
+        }
+        _govermentBuildingImage.sprite = _govermentBuildingSprites[(int)_govermentBuildingState];
+    }
+}
