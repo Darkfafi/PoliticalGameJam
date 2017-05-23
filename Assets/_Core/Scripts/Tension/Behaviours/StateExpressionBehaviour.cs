0c05362dad6ff6a76d6ab35e186623f407c48e4f
Ramses di Perna <15826@ma-web.nl>
2017-05-23 17:08:05 +0200

    Working on tension animtions and implemented some art.

---
+++ Assets/_Core/Scripts/Tension/Behaviours/StateExpressionBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StateExpressionBehaviour : MonoBehaviour
+{
+    private TensionState _currentState;
+
+    [SerializeField]
+    private TensionState _preTensionState = TensionState.Idle;
+
+    public void SetTensionState(TensionState tensionState)
+    {
+        _preTensionState = tensionState;
+        _currentState = tensionState;
+
+        SetStateAnimations(tensionState);
+    }
+
+    protected void Awake()
+    {
+        SetTensionState(TensionState.Idle);
+    }
+
+    protected void Update()
+    {
+        if(_preTensionState != _currentState)
+        {
+            SetTensionState(_preTensionState);
+        }
+    }
+
+    private void SetStateAnimations(TensionState tensionState)
+    {
+        this.transform.DOKill();
+        this.transform.localScale = new Vector3(1, 1, 1);
+
+        switch (tensionState)
+        {
+            case TensionState.Idle:
+                this.transform.DOScale(1.1f, 0.85f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+                break;
+            case TensionState.Pushy:
+
+                this.transform.DOScale(1.1f, 0.85f).SetEase(Ease.OutElastic).OnComplete(()=> {
+
+                    this.transform.DOScale(1, 0.4f).OnComplete(()=> { SetStateAnimations(TensionState.Pushy); });
+                });
+                break;
+            case TensionState.Aggression:
+                this.transform.DOScale(1.15f, 0.35f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InBack);
+                break;
+
+            case TensionState.Outbreak:
+                this.transform.DOScale(1.20f, 0.185f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InBack);
+                break;
+        }
+    }
+}
