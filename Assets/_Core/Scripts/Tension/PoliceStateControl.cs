fd47f91726229611a25589a66c327f761f1a1370
Ramses di Perna <15826@ma-web.nl>
2017-05-23 15:48:44 +0200

    TensionStates, Police states and mob states created.

---
+++ Assets/_Core/Scripts/Tension/PoliceStateControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceStateControl : MonoBehaviour
+{
+    public TensionState TensionState { get; private set; }
+
+    [SerializeField]
+    private TensionManager _tensionManager;
+
+    public static TensionState GetNextNaturalTensionState(TensionState tensionState)
+    {
+        switch(tensionState)
+        {
+            case TensionState.Idle:
+                return TensionState.Pushy;
+            case TensionState.Pushy:
+                return TensionState.Aggression;
+            case TensionState.Aggression:
+                return TensionState.Outbreak;
+            case TensionState.Outbreak:
+                return TensionState.Outbreak;
+        }
+
+        Debug.LogWarning("No Tension return state set for the following state: " + tensionState.ToString());
+        return TensionState.Idle;
+    }
+
+    protected void Awake()
+    {
+        _tensionManager.TensionStateChangedEvent += OnTensionStateChangedEvent;
+    }
+
+    protected void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SetTensionState(GetNextNaturalTensionState(TensionState)); 
+        }
+	}
+
+    private void SetTensionState(TensionState tensionState)
+    {
+        TensionState = tensionState;
+    }
+
+    private void OnTensionStateChangedEvent(TensionState tensionState)
+    {
+        if(tensionState == TensionState.Outbreak)
+        {
+            SetTensionState(TensionState.Outbreak);
+        }
+    }
+}
---
+++ Assets/_Core/Scripts/Tension/TensionManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TensionManager : MonoBehaviour
+{
+    public event TensionStateDelegate TensionStateChangedEvent;
+
+    public float TensionRate { get; private set; }
+    public bool IsRunning { get; private set; }
+
+    private float _tensionMeter = 0;
+    private float _lastTensionMeterValue = 0;
+
+    public void ResumeTensionProgression()
+    {
+        if (IsRunning) { return; }
+        IsRunning = true;
+    }
+
+    public void PauseTensionProgression()
+    {
+        if (!IsRunning) { return; }
+        IsRunning = false;
+    }
+    
+    public void Reset(bool runOnReset)
+    {
+        _tensionMeter = 0;
+        _lastTensionMeterValue = 0;
+        SetTensionRate(0);
+        IsRunning = runOnReset;
+    }
+
+    public void SetTensionRate(float value)
+    {
+        TensionRate = Mathf.Clamp(value, 1, float.MaxValue);
+    }
+
+    public TensionState GetTensionState(float value)
+    {
+        if (value < 20)
+        {
+            return TensionState.Idle;
+        }
+        else if (value < 60)
+        {
+            return TensionState.Pushy;
+        }
+        else if (value < 80)
+        {
+            return TensionState.Aggression;
+        }
+        else
+        {
+            return TensionState.Outbreak;
+        }
+    }
+
+    protected void Awake()
+    {
+        SetTensionRate(TensionRate);
+    }
+
+    protected void Update()
+    {
+        if (!IsRunning) { return; }
+        _lastTensionMeterValue = _tensionMeter;
+        _tensionMeter = Mathf.Clamp(_tensionMeter + Time.deltaTime * TensionRate, 0, 100);
+
+        if(GetTensionState(_lastTensionMeterValue) != GetTensionState(_tensionMeter))
+        {
+            if(TensionStateChangedEvent != null)
+                TensionStateChangedEvent(GetTensionState(_tensionMeter));
+        }
+    }
+}
