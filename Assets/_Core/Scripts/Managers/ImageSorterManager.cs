a2a32a1fa1e89d33c37e279784378bad3ddded37
Rob Verhoef <r.verhoef@hotmail.nl>
2017-05-24 02:50:52 +0200

    Added Y Sorting Manager

---
+++ Assets/_Core/Scripts/Managers/ImageSorterManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ImageSorterManager : MonoBehaviour
+{
+    private void Start ()
+    {
+        List<RectTransform> items = new List<RectTransform>();
+        foreach (RectTransform item in gameObject.GetComponentsInChildren<RectTransform>())
+        {
+            items.Add(item);
+        }
+        List<RectTransform> newItems = items.OrderBy(y => y.position.y).ToList();
+        foreach (RectTransform item in newItems)
+        {
+            item.SetAsFirstSibling();
+        }
+    }
+}
